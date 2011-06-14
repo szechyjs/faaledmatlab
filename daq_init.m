ef63bba8dbb5626e841785d45a8cfc604e61fc1e
Jared Szechy <jared.szechy@gmail.com>
2011-06-13 23:34:00 -0400

    comments cleanup

--- daq_init.m
+++ daq_init.m
@@ -6,6 +6,8 @@ function daq_init()
     if (~exist('ao') || ~exist('ai'))
         
         hw = daqhwinfo('nidaq')
+        
+        % Create an analog output object using Board ID "Dev1".
         ao = analogoutput('nidaq','Dev1')
         addchannel(ao, [0 1]);
 
@@ -13,16 +15,17 @@ function daq_init()
         ai = analoginput('nidaq','Dev5');
 
         % Data will be acquired from hardware channel 0 and 1
-        %these represent the the yes and no buttons
+        % these represent the the yes and no buttons
         addchannel(ai, [0 1]);
+        
         % Set the sample rate and samples per trigger
-        %at a sample collection rate of 100 samples per second, collecting 500
-        %samples will be equivalent to 5 seconds of data collection
+        % at a sample collection rate of 100 samples per second, collecting 500
+        % samples will be equivalent to 5 seconds of data collection
         ai.SampleRate = 100;
         ai.SamplesPerTrigger = 500;
     end
 
-    %initialize the LED with 0V --> light will be off
+    % initialize the LED with 0V --> light will be off
     putsample(ao, [0 5])
     
 end
