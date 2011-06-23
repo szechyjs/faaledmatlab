7091254ac907e262636969276f1f8e5b19518dc6
Jared Szechy <jared.szechy@gmail.com>
2011-06-22 21:29:16 -0400

    -add train pulse prompts to config run

--- SDT_Config_Run.m
+++ SDT_Config_Run.m
@@ -4,8 +4,10 @@
 clear all; close all; clc
 
 % Default pulse widths
-pulses = [50 140];       % 26, 50, 90, 140, 226, 500, 1000
-num_pulses = length(pulses);
+pulses1 = [50 140];       % 26, 50, 90, 140, 226, 500, 1000
+pulses2 = [500 1000];
+pulses_train = [67 115; 140 140];
+num_train_pulses = length(pulses_train);
 
 % Time approximation variables
 time_pre = 10;
@@ -17,8 +19,18 @@ time_total = 0;
 run_pre = questdlg('Run pre-test?', 'Pre-test', 'Yes', 'No', 'Yes');
 run_steady = questdlg('Run steady state?', 'Steady State', 'Yes', 'No', 'Yes');
 run_pulse = questdlg('Run pulse test(s)?', 'Pulse Tests', 'Yes', 'No', 'Yes');
+run_train = questdlg('Run pulse train test(s)?', 'Pulse Train Tests', 'Yes', 'No', 'No');
 
 if (strcmp(run_pulse, 'Yes'))
+    ses_num = input('Enter session number (1): ');
+    if (ses_num == 1)
+        pulses = pulses1;
+    elseif (ses_num == 2)
+        pulses = pulses2;
+    else
+        pulses = pulses1;
+    end
+    num_pulses = length(pulses);
     disp('The current pulse widths are: (ms)');
     disp(pulses);
     chg_pulses = input('To change them press enter "y" : ', 's');
@@ -27,7 +39,22 @@ if (strcmp(run_pulse, 'Yes'))
        pulses = [];
        num_pulses = input('Enter the number of pulses to test: ');
        for i=1:num_pulses
-           pulses(length(pulses)+1) = input(sprintf('Enter the pulse width (ms) for test %d : ', i));
+           pulses(i) = input(sprintf('Enter the pulse width (ms) for test %d : ', i));
+       end
+    end
+end
+
+if (strcmp(run_train, 'Yes'))
+    disp('The current pulse widths are: (ms)');
+    disp(pulses_train);
+    chg_train = input('To change them press enter "y" : ', 's');
+    
+    if (strcmp(chg_train, 'y'))
+       pulses_train = [];
+       num_train_pulses = input('Enter the number of pulse trains to test: ');
+       for i=1:num_train_pulses
+           pulses_train(i,1) = input(sprintf('Enter the pulse width (ms) for test %d : ', i));
+           pulses_train(i,2) = input(sprintf('Enter the delay width (ms) for test %d : ', i));
        end
     end
 end
@@ -42,6 +69,9 @@ end
 if (strcmp(run_pulse, 'Yes'))
     time_total = time_total + (num_pulses * time_pulse);
 end
+if (strcmp(run_train, 'Yes'))
+    time_total = time_total + (num_train_pulses * time_pulse);
+end
 
 % Display test configuration
 disp(' ');
@@ -54,6 +84,11 @@ fprintf('Pulse: %s\n', run_pulse);
 if (strcmp(run_pulse, 'Yes'))
     disp('Pulses to test: (ms)');
     disp(pulses);
+end
+fprintf('Pulse: %s\n', run_train);
+if (strcmp(run_train, 'Yes'))
+    disp('Pulse trains to test: (ms)');
+    disp(pulses_train);
 end
 fprintf('Estimated test time: %g minutes\n', time_total);
 disp('********************************');
@@ -93,3 +128,13 @@ if (strcmp(run_pulse,'Yes'))
     disp('Pulse Testing Complete!');
 end
 
+%% Run pulse train tests
+if (strcmp(run_train,'Yes'))
+    disp('Starting Pulse Train Tests');
+    for i=1:num_train_pulses
+       fprintf('Testing %dms Pulses, %dms Delay...\n', pulses_train(i,1), pulses_train(i,2));
+       train_test(pulses_train(i,1), pulses_train(i,2), subject_number); 
+       fprintf('%dms Pulse Train Test Complete!', pulses_train(i));
+    end
+    disp('Pulse Train Testing Complete!');
+end
